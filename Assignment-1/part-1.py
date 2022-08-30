dc2fbb2e43fe70a01e17cbcdcee1ec8db3cde20e
HAYJAKS <akshit.26001@gmail.com>
2022-08-30 16:08:01 +0530

    Image reading

--- Assignment-1/part-1.py
+++ Assignment-1/part-1.py
@@ -1,5 +1,9 @@
-def foo():
-    return 0
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
 
-
-print(foo())
+img = np.array(cv.imread("lena256.png"))
+img_yuv = cv.cvtColor(img, cv.COLOR_BGR2YUV)
+y,u,v = cv.split(img_yuv)
+cv.imshow('y',y)
+cv.waitKey(0)
