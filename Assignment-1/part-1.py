be41757687755f95b903427ed544a8b640d8060c
Arnav Singh <arnavsingh1359@gmail.com>
2022-09-12 03:29:05 +0530

    Revert "wls_final working"
    This reverts commit 3f0db108ac64fd86d2b52b8f22f818a2caff3b49.

--- Assignment-1/part-1.py
+++ Assignment-1/part-1.py
@@ -9,7 +9,7 @@ from scipy import stats
 from scipy.sparse import spdiags
 from scipy.sparse.linalg import spsolve
 
-img = np.array(cv.imread("Pictures/part1/img_6.png"))
+img = np.array(cv.imread("Pictures/part1/img_1.png"))
 
 # lum, alpha, beta = np.array_split(cv.cvtColor(img, cv.COLOR_BGR2LAB), 3, axis=2)
 # hue, sat, value = np.array_split(cv.cvtColor(img, cv.COLOR_BGR2HSV), 3, axis=2)
@@ -43,7 +43,7 @@ def detect_faces(image, scale_factor=1.01, min_neighbor=6):
     return faces
 
 
-def WLS_filter(img, lambda_ = 0.1,alpha = 1.2, eps = 10^-4):
+def WLS_filter(img, lambda_ = 0.4,alpha = 1, eps = 10^-4):
     image = img/255.0
 
     s = image.shape
@@ -51,53 +51,52 @@ def WLS_filter(img, lambda_ = 0.1,alpha = 1.2, eps = 10^-4):
     k = np.prod(s)
 
     dy = np.diff(image, 1, 0)
-    dy = -lambda_ / (np.absolute(dy) ** alpha + eps)
+    dy = -lambda_ / (np.absolute(dy)**alpha + eps)
     last_y = np.zeros((s[1],))
     dy = np.vstack((dy, last_y))
-    dy = dy.flatten('F')
+    dy = dy.flatten()
 
     dx = np.diff(image, 1, 1)
     dx = -lambda_ / (np.absolute(dx) ** alpha + eps)
     last_x = np.zeros((s[0],))
     dx = np.hstack((dx, last_x[:, np.newaxis]))
-    dx = dx.flatten('F')
+    dx = dx.flatten()
 
-    a = spdiags(np.vstack((dx, dy)), [-s[0], -1], k, k)
+    a = spdiags(np.vstack((dx, dy)), [-s[0], -1], k, k, format="csr")
 
     d = 1 - (dx + np.roll(dx, s[0]) + dy + np.roll(dy, 1))
     a = a + a.T + spdiags(d, 0, k, k )
-    _out = spsolve(a, image.flatten('F')).reshape(s[::-1])
+    _out = spsolve(a, image.flatten()).reshape(s[::-1])
 
-    base  = np.rollaxis(_out,1)*255
-    out = np.clip(base, 0, 255)
-    detail = image - out
-    np.clip(detail, 0, 255, out=detail)
-    return out, detail
+    out = np.rollaxis(_out,1)*255.0
+    detail_ = image - out
+    detail = np.rollaxis(detail_,1)*255.0
+    return out, detail 
 
-def eacp(F, L, W= None, lambda_=0.2, alpha=0.3, eps=1e-4):
-    if W is None:
+def eacp(F, Log_I, W=None, lambda_=0.2, alpha=0.3, eps=1e-4):
+    if W == None:
         W= np.ones(F.shape)
-    f= F.flatten('F')
-    w = W.flatten('F')
-    s = L.shape
+    f= F.flatten()
+    w = W.flatten()
+    s = Log_I.shape
 
     k = np.prod(s)
     # L_i - L_j along y axis
     dy = np.diff(L, 1, 0)
     dy = -lambda_ / (np.absolute(dy) ** alpha + eps)
     dy = np.vstack((dy, np.zeros(s[1], )))
-    dy = dy.flatten('F')
+    dy = dy.flatten()
     # L_i - L_j along x axis
     dx = np.diff(L, 1, 1)
     dx = -lambda_ / (np.absolute(dx) ** alpha + eps)
     dx = np.hstack((dx, np.zeros(s[0], )[:, np.newaxis]))
-    dx = dx.flatten('F')
+    dx = dx.flatten()
     # A case: j \in N_4(i)  (neighbors of diagonal line)
     a = spdiags(np.vstack((dx, dy)), [-s[0], -1], k, k)
     # A case: i=j   (diagonal line)
     d = w - (dx + np.roll(dx, s[0]) + dy + np.roll(dy, 1))
     a = a + a.T + spdiags(d, 0, k, k) # A: put together
-    f = spsolve(a, w*f).reshape(s[::-1]) # slove Af  =  b =w*g and restore 2d
+    f = spsolve(a, w*g).reshape(s[::-1]) # slove Af  =  b =w*g and restore 2d
     A = np.rollaxis(f,1)
     # A = np.clip( _out*255.0, 0, 255).astype('uint8')
     return A
@@ -126,22 +125,18 @@ def face_correction(image):
     beta = beta[:, :, 0]
 
     I_out,detail = WLS_filter(lum)
-
     skin_mask = detected_skin(image)
     #skin patching
     _h,_w = image.shape[:2]
     _kernel = cv.getStructuringElement(cv.MORPH_RECT,(int(_h/48),int(_w/48)))
     skin_mask_closed = cv.morphologyEx(skin_mask,cv.MORPH_CLOSE,_kernel)
-    
+    plt.imshow(skin_mask_closed,cmap ='gray')
     #face_detection
     face = detect_faces(image, 1.001, 5)
     #all faces
     faces = [lum[y:y + h, x:x + w] for (x, y, w, h) in face]
-    W = np.zeros(lum.shape)
-    A = np.ones(lum.shape, dtype="float")
-    B = np.ones(lum.shape)
     #face_correction a)sidelight and b)exposure
-    for index,(x, y, w, h) in enumerate(face):
+    for index,(x, y, w, h) in enumerate(faces):
         #sidelight correction
         skin_in_face = skin_mask_closed[y:y+h, x:x+w]
         data, bins = exposure.histogram(lum[y:y + h, x:x + w], nbins=256)
@@ -152,39 +147,28 @@ def face_correction(image):
         # plt.plot(intensity, density)
         maxima = argrelextrema(density, np.greater)
         minima = argrelextrema(density, np.less)
-        d, b, m = is_bimodal(intensity, density)        
-        #global W
+        d, b, m = is_bimodal(intensity, density) 
+        print(index,d,b,m)
+        A = np.ones(lum.shape)
         if d and b and m:
             f = (b - d) / (m - d)
             r = 0
             while r < w:
                 s = 0
                 while s < h:
-                    if skin_in_face[r, s] and (lum[y + r, x + s] < m):
+                    if skin_in_face[r, s] and lum[y + r, x + s] < m:
                         A[y + r, x + s] = f
                     s += 1
                 r += 1
-            miu = (lum[A == f]).mean()
-            sig = 255 * 3  # manually set, 3*sig = 120 close to 255/2
-            W = np.exp(-(lum - miu) ** 2 / sig ** 2)
-            W[...] = 1 - W[...]
-            W[...] = 1.        
-    A_after = eacp(A,lum,W)
-    I_out *=A_after
-    # #exposure_correction
-    # for index,(x, y, w, h) in enumerate(face):
-    #     skin_in_face = skin_mask_closed[y:y+h, x:x+w]      
-    #     cumsum = cv.calcHist([skin_in_face], [0], None, [255], [1, 256]).T.ravel().cumsum()
-    #     p = np.searchsorted(cumsum, cumsum[-1] * 0.75)
-    #     if p < 120:
-    #         f = (120 + p) / ((2 * p) + 1e-6)
-    #         B[y:y + h, x:x + w]= f 
-    #         B = eacp(B, lum)
-    #         I_out *= B       
-                        
-    final = I_out + detail
+
+
+        A_after = eacp(A,lum,W)
+        I_out = np.multiply(I_out,A_after)
+
+        #exposure_correction
+
+    final = out + detail
     final = np.stack((final, alpha, beta), axis=2)
-    final = final.astype("uint8")
     final_bgr = cv.cvtColor(final, cv.COLOR_Lab2BGR)
     cv.imshow("Final", final_bgr)
     cv.waitKey(0)
